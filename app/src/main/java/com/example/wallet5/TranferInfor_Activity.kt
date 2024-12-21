dd2c557d7585ef608c78686b8f2c9204849ea4aa
TuongDuyHuong <khongphaihuong2k4@gmail.com>
2024-12-21 21:43:12 +0700

    Wallet

--- app/src/main/java/com/example/wallet5/TranferInfor_Activity.kt
+++ app/src/main/java/com/example/wallet5/TranferInfor_Activity.kt
@@ -3,9 +3,16 @@ package com.example.wallet5
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_tranfer_infor.*
+import kotlinx.android.synthetic.main.activity_tranfer_infor.btnBackMenuTranfer
+import kotlinx.android.synthetic.main.activity_tranfer_infor.btnNextToOTP
+import kotlinx.android.synthetic.main.otp_dialog.*
 
 class TranferInfor_Activity : AppCompatActivity() {
+    lateinit var dialog : AlertDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tranfer_infor)
@@ -14,5 +21,29 @@ class TranferInfor_Activity : AppCompatActivity() {
             val intent = Intent(this, Tranfer_Activity::class.java)
             startActivity(intent)
         }
+        btnNextToOTP.setOnClickListener {
+            showDialogOTP()
+        }
+    }
+
+    private fun showDialogOTP() {
+        val build = AlertDialog.Builder(this)
+        val view  = layoutInflater.inflate(R.layout.otp_dialog, null)
+        build.setView(view)
+        val btnCloseDialog= view.findViewById<ImageButton>(R.id.btnCloseDialog)
+        btnCloseDialog.setOnClickListener {
+            dialog.dismiss()
+        }
+        val btnBackTranfer = view.findViewById<Button>(R.id.btnBackTranfer)
+        btnBackTranfer.setOnClickListener { dialog.dismiss() }
+        val btnNextToTranfer = view.findViewById<Button>(R.id.btnNextToTranfer)
+        btnNextToTranfer.setOnClickListener {
+            dialog.dismiss()
+            val intent = Intent(this, Tranferred_Activity::class.java)
+            startActivity(intent)
+
+        }
+        dialog =  build.create()
+        dialog.show()
     }
 }
